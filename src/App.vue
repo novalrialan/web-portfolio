32c0ebcd806c877fec3bbf6b1ba92693fdb93adc
Noval Rialan <48094771+novalrialan@users.noreply.github.com>
2024-02-21 13:06:17 +0700

    setup web-portfolio

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+// import GreetingMessage  from '@/components/GreetingMessage.vue';
+import Navbar        from './assets/Navbar.vue';
+import Container     from './assets/Container.vue';
+import ContainerBody from './assets/Container-body.vue';
+</script>
+
+<template>
+  <div class="max-w-10xl mx-auto">
+    <Navbar/>
+  </div>
+  <div class="max-w-11xl mx-auto">
+    <Container/>
+  </div>
+  <div class="bg-indigo-950 h-70" >
+    <ContainerBody/>
+  </div>
+
+
+
+  
+</template>
---
+++ src/assets/GreetingMessage.vue
@@ -0,0 +1,27 @@
+<script setup>
+// const props = defineProps({
+//     firstName: String,
+//     lastName: String
+// })
+// ---------------------
+// import { defineEmits } from 'vue';
+
+// const emits = defineEmits(['greet'])
+
+// function greetAlert() {
+//     emits('greet','Noval Rialan')
+// }
+// ---------------------
+import { ref } from 'vue'
+let joke = ref({})
+
+await fetch('https://api.chucknorris.io/jokes/random')
+    .then((response) => response.json())
+.then((data)=> joke = data)
+</script>
+
+<template>
+    <!-- <h2>Hello {{ props.firstName }} {{ props.lastName }}</h2> -->
+    <!-- <button @click="greetAlert">Greet Me</button> -->
+    <p>{{ joke.value }}</p>
+</template>
