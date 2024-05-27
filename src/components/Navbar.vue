810d1bcd2283dda38ea6eb27df8e19ce26a476be
Noval Rialan <48094771+novalrialan@users.noreply.github.com>
2024-05-27 21:39:12 +0700

    fix

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -99,43 +99,68 @@
       </div>
     </div>
   </nav>
-
   <!-- end Navigation -->
-
-  <div class="bg-gradient-to-r from-blue-500 w-full mt-10">
-    <br />
-    <br />
-    <div class="container mx-auto grid gap-8 grid-cols-1 md:grid-cols-2 mr2">
-      <!-- content text -->
-      <div class="w-full h-full flex justify-center items-center">
-        <h1 id="typewriter" class="text-4xl text-white"></h1>
+  <div
+    id="container"
+    class="p-20 w-auto flex px-24 justify-center relative font-poppins bg-black"
+  >
+    <div
+      id="container "
+      class="p-20 sm:p-16 lg:p-24 xl:p-20 w-auto flex flex-col md:flex-row px-4 sm:px-8 md:px-24 lg:px-24 xl:px-24 relative"
+    >
+      <div class="relative">
+        <img
+          class="bg-sky-500 rounded-full min-w-[100px] w-90 h-90 md:w-auto md:h-auto m-8 md:content-center"
+          src="/src/assets/images/person.png"
+          alt="image of myself"
+        />
       </div>
-      <!-- content Text -->
-
-      <div class="flex justify-center">
-        <div class="card-person relative h-100 w-100">
-          <img
-            class="rounded-full md:h-auto sm:w-auto bg-gradient-to-r from-slate-800 to-slate-500"
-            src="/src/assets/images/person.png"
-            alt="NovalRialan"
-            srcset=""
-          />
-          <!-- <img
-            class="bg-gradient-to-r from-slate-800 to-slate-500"
-            src="/src/assets/images/person.png"
-            alt="John"
-            style="width: 100%"
-          />
-          <h1 class="text-3xl font-sans">Noval Rialan</h1>
-          <p class="text-2px text-stone-500">
-            Coder & Software engineer, Junior
-          </p>
-          <p>Immanuel Kristen University</p> -->
+      <div class="w-full sm:w-[70%] md:w-[60%] lg:w-[50%]">
+        <h1 class="text-white font-bold text-3xl mt-6 mb-10 m-8">
+          Hey I'm, Noval Rialan
+        </h1>
+        <p
+          class="text-justify text-white w-full sm:w-[35rem] md:w-[30rem] lg:w-[35rem] m-8"
+        >
+          I'm Noval, passion for web development. My tech journey started with
+          HTML, CSS, and JavaScript, and I was hooked by the thrill of crafting
+          dynamic, interactive websites. As I grew, Node.js, vue.jS, and ReactJS
+          became my go-to tools for building scalable applications. Feel free to
+          connect if you have questions, collaboration ideas, or just want to
+          discuss the latest in web development !
+        </p>
+        <div
+          id="social"
+          class="flex flex-wrap justify-start items-center gap-2 m-7"
+        >
+          <a
+            rel="noopener"
+            target="_blank"
+            href="https://www.github.com/novalrialan"
+            class="bg-gray-800 rounded-lg p-5 w-64 flex items-center gap-2 text-white"
+            ><img
+              class="mr-2 hover:scale-105 transition duration-300 ease-in-out"
+              src="https://ucarecdn.com/1f465c47-4849-4935-91f4-29135d8607af/github2.svg"
+              width="20px"
+              height="20px"
+              alt="Github"
+            /><span>Visit my Github</span></a
+          ><a
+            rel="noopener"
+            target="_blank"
+            href="https://www.linkedin.com/in/noval-rialan"
+            class="bg-gray-800 rounded-lg p-5 w-64 flex items-center gap-2 text-white"
+            ><img
+              class="mr-2 hover:scale-105 transition duration-300 ease-in-out"
+              src="https://ucarecdn.com/95eebb9c-85cf-4d12-942f-3c40d7044dc6/linkedin.svg"
+              width="20px"
+              height="20px"
+              alt="LinkedIn"
+            /><span>Follow me on Linkedin</span></a
+          >
         </div>
       </div>
     </div>
-
-    <br />
   </div>
 </template>
 <script setup>
@@ -195,15 +220,19 @@ function type() {
   }
   setTimeout(type, 300);
 }
+
+// const observer = new IntersectionObserver((entires) => {
+//   entires.forEach((entry) => {
+//     console.log(entry);
+//     if (entry.isIntersecting) {
+//       entry.target.classList.add("show");
+//     } else {
+//       entry.target.classList.remove("show");
+//     }
+//   });
+// });
+
+// const hiddenElements = document.querySelectorAll(".hidden");
 </script>
 
-<style>
-/* .card-person {
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-  max-width: 300px;
-  margin: auto;
-  text-align: center;
-  font-family: arial;
-  background-color: white;
-} */
-</style>
+
--- src/main.js
+++ src/main.js
@@ -2,10 +2,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/style.css'
-import { data } from 'autoprefixer'
+
 
 const app = createApp(App)
 import './index.css'
+
 
 app.mount('#app'
 
